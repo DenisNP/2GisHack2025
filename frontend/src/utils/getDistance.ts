c199a0e72aca4208cdaeb59b9944e6701b6e6fc3
Denis Peshekhonov <denis@peshekhonov.ru>
2025-10-04 11:45:13 +0300

    сервисные функции

---
+++ frontend/src/utils/getDistance.ts
@@ -0,0 +1,20 @@
+import { GeoPoint } from '../types/GeoPoint';
+
+export default (p1: GeoPoint, p2: GeoPoint): number => {
+    const R = 6378137; // Earth’s mean radius in meter
+    const dLat = rad(p2.lat - p1.lat);
+    const dLong = rad(p2.lng - p1.lng);
+    const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(rad(p1.lat)) *
+            Math.cos(rad(p2.lat)) *
+            Math.sin(dLong / 2) *
+            Math.sin(dLong / 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c;
+};
+
+const rad = function(x: number): number {
+    return x * Math.PI / 180;
+};
