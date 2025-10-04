3c325f127cf191df871dcdce2829f336bd286d08
a.larchenko <a.larchenko@2gis.ru>
2025-10-04 19:39:46 +0700

    поправил app

---
+++ frontend/src/components/ZoneDrawer/ZoneDrawer.types.ts
@@ -0,0 +1,20 @@
+import { GeoPoint } from "../../types/GeoPoint";
+import { ZoneType } from "../../types/Zone"
+
+export type ZoneDrawerProps = {
+    type: ZoneType,
+    zones: ZoneData[];
+    onZonesChanged: (polygons: ZoneData[]) => void;
+}
+
+export type ZoneData = { 
+    id: number; 
+    coords: GeoPoint[][] 
+};
+
+export type ZonePolygon = {
+    id: number;
+    instance: any; 
+    coords: number[][][]; 
+    rgb?: { r:number; g:number; b:number }
+}
