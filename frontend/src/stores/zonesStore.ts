454c7f34b2ca72f64f52369d455b9ee6573c1db5
a.larchenko <a.larchenko@2gis.ru>
2025-10-05 03:37:40 +0700

    event для добавления произвольных зон

--- frontend/src/stores/zonesStore.ts
+++ frontend/src/stores/zonesStore.ts
@@ -1,19 +1,35 @@
-import { combine, createEvent, createStore, restore } from "effector";
+import { combine, createEvent, createStore, restore, sample } from "effector";
 import { ZoneData } from "../components/ZoneDrawer";
 import { SidewalkData } from "../components/UrbanDrawer";
 import { Zone, ZoneType } from "../types/Zone";
 import { MapStore, stores as mapStores } from "./mapStore";
 import { projectPoint } from "../utils/pointsProjection";
+import { GeoPoint } from "../types/GeoPoint";
+
+type AddZoneProps = {
+    type: ZoneType,
+    coords: GeoPoint[] 
+}
 
 const setAvailableZones = createEvent<ZoneData[]>()
+const addAvailabelZone = createEvent<ZoneData>()
 const setRestrictedZones = createEvent<ZoneData[]>()
+const addRestrictedZone = createEvent<ZoneData>()
 const setUrbanZones = createEvent<ZoneData[]>()
+const addUrbanZone = createEvent<ZoneData>()
 const setSidewalks = createEvent<SidewalkData[]>()
 const incrementZoneId = createEvent<void>()
+const addZone = createEvent<AddZoneProps>()
 
-const $availableZones = restore(setAvailableZones, []) 
-const $restrictedZones = restore(setRestrictedZones, [])
-const $urbanZones = restore(setUrbanZones, [])
+const $availableZones = restore(setAvailableZones, []).on(addAvailabelZone, (state, zone) => [
+    ...state,
+    zone])
+const $restrictedZones = restore(setRestrictedZones, []).on(addRestrictedZone, (state, zone) => [
+    ...state,
+    zone])
+const $urbanZones = restore(setUrbanZones, []).on(addUrbanZone, (state, zone) => [
+    ...state,
+    zone])
 const $sidewalks = restore(setSidewalks, [])
 const $newZoneId = createStore(1).on(incrementZoneId, (state) => state + 1);
 
@@ -48,6 +64,29 @@ const $allZones = combine([$availableZones, $restrictedZones, $urbanZones, $side
     ]
 })
 
+sample({
+    clock: addZone,
+    source: $newZoneId,
+    filter: (_, data)=> data.type !== ZoneType.Available,
+    fn: (newZoneId, {coords}): ZoneData =>({id: newZoneId, coords}),
+    target: [addAvailabelZone, incrementZoneId]
+})
+
+sample({
+    clock: addZone,
+    source: $newZoneId,
+    filter: (_, data)=> data.type !== ZoneType.Restricted,
+    fn: (newZoneId, {coords}): ZoneData =>({id: newZoneId, coords}),
+    target: [addRestrictedZone, incrementZoneId]
+})
+
+sample({
+    clock: addZone,
+    source: $newZoneId,
+    filter: (_, data)=> data.type !== ZoneType.Urban,
+    fn: (newZoneId, {coords}): ZoneData =>({id: newZoneId, coords}),
+    target: [addUrbanZone, incrementZoneId]
+})
 
 export const stores = {
     $availableZones,
@@ -64,4 +103,5 @@ export const events = {
     setUrbanZones,
     setSidewalks,
     incrementZoneId,
+    addZone
 }
