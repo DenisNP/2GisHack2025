e81166fd67fccd107e594dccd3be6bae73d985bd
Kirill Poltoranin <kirillpoltoranin@gmail.com>
2025-10-05 19:34:06 +0700

    Сделал еще один Алгоритм

---
+++ backend/2GisHack2025/AntAlgorithm/AntColonyAlgorithmRemake.cs
@@ -0,0 +1,352 @@
+﻿using and.Models;
+using Microsoft.Extensions.Options;
+
+namespace AntAlgorithm;
+
+public class AntColonyAlgorithmRemake
+{
+    private static Random random = new Random(0);
+    
+    private readonly double _alpha;
+    private readonly double _beta;
+    private readonly double _gamma;
+    private readonly double _evaporation;
+    private readonly double _q;
+
+    private class IdDistMap
+    {
+        public int FromId { get; set; }
+        public int ToId { get; set; }
+        public double Dist { get; set; }
+    }
+
+    public AntColonyAlgorithmRemake(IOptions<AntColonyConfiguration> configuration)
+    {
+        _alpha = configuration.Value.Alpha;
+        _beta = configuration.Value.Beta;
+        _gamma = configuration.Value.Gamma;
+        _evaporation = configuration.Value.Evaporation;
+        _q = configuration.Value.Q;
+    }
+
+    public List<Path> Calculate(Edge[] edges)
+    {
+        var numCities = edges.Length;
+        var numAnts = 100;
+        var maxTime = 1000;
+
+        var dists = MakeGraphDistances(numCities, edges);
+        var ants = InitAnts(numAnts, numCities);
+        var pheromones = InitPheromones(numCities);
+
+        var time = 0;
+        while (time < maxTime)
+        {
+            UpdateAnts(ants, pheromones, dists);
+            UpdatePheromones(pheromones, ants, dists);
+
+            time += 1;
+        }
+
+        return new List<Path>();
+    }
+
+    private void UpdatePheromones(double[][] pheromones, int[][] ants, IdDistMap[,] dists)
+    {
+        for (var i = 0; i <= pheromones.Length - 1; i++)
+        {
+            for (var j = i + 1; j <= pheromones[i].Length - 1; j++)
+            {
+                for (var k = 0; k <= ants.Length - 1; k++)
+                {
+                    var length = Length(ants[k], dists);
+                    // length of ant k trail
+                    var decrease = (1.0 - _evaporation) * pheromones[i][j];
+                    var increase = 0.0;
+                    if (EdgeInTrail(i, j, ants[k]) == true)
+                    {
+                        increase = (_q / length);
+                    }
+
+                    pheromones[i][j] = decrease + increase;
+
+                    if (pheromones[i][j] < 0.0001)
+                    {
+                        pheromones[i][j] = 0.0001;
+                    }
+                    else if (pheromones[i][j] > 100000.0)
+                    {
+                        pheromones[i][j] = 100000.0;
+                    }
+
+                    pheromones[j][i] = pheromones[i][j];
+                }
+            }
+        }
+    }
+    
+    private static bool EdgeInTrail(int cityX, int cityY, int[] trail)
+    {
+        // are cityX and cityY adjacent to each other in trail[]?
+        var lastIndex = trail.Length - 1;
+        var idx = IndexOfTarget(trail, cityX);
+
+        if (idx == 0 && trail[1] == cityY)
+        {
+            return true;
+        }
+        else if (idx == 0 && trail[lastIndex] == cityY)
+        {
+            return true;
+        }
+        else if (idx == 0)
+        {
+            return false;
+        }
+        else if (idx == lastIndex && trail[lastIndex - 1] == cityY)
+        {
+            return true;
+        }
+        else if (idx == lastIndex && trail[0] == cityY)
+        {
+            return true;
+        }
+        else if (idx == lastIndex)
+        {
+            return false;
+        }
+        else if (trail[idx - 1] == cityY)
+        {
+            return true;
+        }
+        else if (trail[idx + 1] == cityY)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void UpdateAnts(int[][] ants, double[][] pheromones, IdDistMap[,] dists)
+    {
+        var numCities = pheromones.Length;
+        for (var k = 0; k <= ants.Length - 1; k++)
+        {
+            var start = random.Next(0, numCities);
+            var newTrail = BuildTrail(k, start, pheromones, dists);
+            ants[k] = newTrail;
+        }
+    }
+
+    private int[] BuildTrail(int k, int start, double[][] pheromones, IdDistMap[,] dists)
+    {
+        var numCities = pheromones.Length;
+        var trail = new int[numCities];
+        var visited = new bool[numCities];
+        trail[0] = start;
+        visited[start] = true;
+        for (var i = 0; i <= numCities - 2; i++)
+        {
+            var cityX = trail[i];
+            var next = NextCity(cityX, visited, pheromones, dists);
+            trail[i + 1] = next;
+            visited[next] = true;
+        }
+        return trail;
+    }
+
+    private int NextCity(int cityX, bool[] visited, double[][] pheromones, IdDistMap[,] dists)
+    {
+        // for ant k (with visited[]), at nodeX, what is next node in trail?
+        var probs = MoveProbs(cityX, visited, pheromones, dists);
+
+        var cumul = new double[probs.Length + 1];
+        for (var i = 0; i <= probs.Length - 1; i++)
+        {
+            cumul[i + 1] = cumul[i] + probs[i];
+            // consider setting cumul[cuml.Length-1] to 1.00
+        }
+
+        var p = random.NextDouble();
+
+        for (var i = 0; i <= cumul.Length - 2; i++)
+        {
+            if (p >= cumul[i] && p < cumul[i + 1])
+            {
+                return i;
+            }
+        }
+        throw new Exception("Failure to return valid city in NextCity");
+    }
+
+    private double[] MoveProbs(int cityX, bool[] visited, double[][] pheromones, IdDistMap[,] dists)
+    {
+        // for ant k, located at nodeX, with visited[], return the prob of moving to each city
+        var numCities = pheromones.Length;
+        var taueta = new double[numCities];
+        // inclues cityX and visited cities
+        var sum = 0.0;
+        // sum of all tauetas
+        // i is the adjacent city
+        for (var i = 0; i <= taueta.Length - 1; i++)
+        {
+            if (i == cityX)
+            {
+                taueta[i] = 0.0;
+                // prob of moving to self is 0
+            }
+            else if (visited[i])
+            {
+                taueta[i] = 0.0;
+                // prob of moving to a visited city is 0
+            }
+            else
+            {
+                taueta[i] = Math.Pow(pheromones[cityX][i], _alpha) * Math.Pow((1.0 / Distance(cityX, i, dists).Dist), _beta);
+                // could be huge when pheromone[][] is big
+                if (taueta[i] < 0.0001)
+                {
+                    taueta[i] = 0.0001;
+                }
+                else if (taueta[i] > (double.MaxValue / (numCities * 100)))
+                {
+                    taueta[i] = double.MaxValue / (numCities * 100);
+                }
+            }
+            sum += taueta[i];
+        }
+
+        var probs = new double[numCities];
+        for (var i = 0; i <= probs.Length - 1; i++)
+        {
+            probs[i] = taueta[i] / sum;
+            // big trouble if sum = 0.0
+        }
+        return probs;
+    }
+
+    private IdDistMap Distance(int cityX, int cityY, IdDistMap[,] dists)
+    {
+        return dists[cityX, cityY];
+    }
+
+    private double[][] InitPheromones(int numCities)
+    {
+        var pheromones = new double[numCities][];
+        for (var i = 0; i <= numCities - 1; i++)
+        {
+            pheromones[i] = new double[numCities];
+        }
+        for (var i = 0; i <= pheromones.Length - 1; i++)
+        {
+            for (var j = 0; j <= pheromones[i].Length - 1; j++)
+            {
+                pheromones[i][j] = 0.01;
+                // otherwise first call to UpdateAnts -> BuiuldTrail -> NextNode -> MoveProbs => all 0.0 => throws
+            }
+        }
+        return pheromones;
+    }
+
+    private int[][] InitAnts(int numAnts, int numCities)
+    {
+        var ants = new int[numAnts][];
+        for (var k = 0; k < numAnts - 1; k++)
+        {
+            var start = random.Next(0, numCities);
+            ants[k] = RandomTrail(start, numCities);
+        }
+
+        return ants;
+    }
+
+    private int[] RandomTrail(int start, int numCities)
+    {
+        // helper for InitAnts
+        var trail = new int[numCities];
+
+        // sequential
+        for (var i = 0; i <= numCities - 1; i++)
+        {
+            trail[i] = i;
+        }
+
+        // Fisher-Yates shuffle
+        for (var i = 0; i <= numCities - 1; i++)
+        {
+            var r = random.Next(i, numCities);
+            var tmp = trail[r];
+            trail[r] = trail[i];
+            trail[i] = tmp;
+        }
+
+        var idx = IndexOfTarget(trail, start);
+        // put start at [0]
+        var temp = trail[0];
+        trail[0] = trail[idx];
+        trail[idx] = temp;
+
+        return trail;
+    }
+    
+    private static int IndexOfTarget(int[] trail, int target)
+    {
+        // helper for RandomTrail
+        for (var i = 0; i <= trail.Length - 1; i++)
+        {
+            if (trail[i] == target)
+            {
+                return i;
+            }
+        }
+        throw new Exception("Target not found in IndexOfTarget");
+    }
+    
+    private double Length(int[] trail, IdDistMap[,] dists)
+    {
+        // total length of a trail
+        var result = 0.0;
+        for (var i = 0; i <= trail.Length - 2; i++)
+        {
+            result += Distance(trail[i], trail[i + 1], dists).Dist;
+        }
+        return result;
+    }
+
+    private IdDistMap[,] MakeGraphDistances(int numCities, Edge[] edges)
+    {
+        var dists = new IdDistMap[numCities, numCities];
+
+        for (var i = 0; i < numCities; i++)
+        {
+            var fromCoords = edges[i].From;
+            var toCoords = edges.Where(it => it.From.Id == fromCoords.Id).Select(it => it.To).ToList();
+            
+            for (var j = 0; j < toCoords.Count; j++)
+            {
+                var dist = MathExtensions.CalculateDistance(fromCoords.Point, toCoords[j].Point);
+                var saveValueij = new IdDistMap
+                {
+                    FromId = fromCoords.Id,
+                    ToId = toCoords[j].Id,
+                    Dist = dist,
+                };
+                
+                var saveValueji = new IdDistMap
+                {
+                    FromId = toCoords[j].Id,
+                    ToId = fromCoords.Id,
+                    Dist = dist,
+                };
+
+
+                dists[i, j] = saveValueij;
+                dists[j, i] = saveValueji;
+            }
+        }
+        
+        return dists;
+    }
+}
---
+++ backend/2GisHack2025/AntAlgorithm/AntColonyRemake2.cs
@@ -0,0 +1,371 @@
+﻿using and.Models;
+using Microsoft.Extensions.Options;
+
+namespace AntAlgorithm;
+
+public class Ant
+{
+    public int Id { get; set; }
+    public Poi CurrentPosition { get; set; }
+    public Poi StartPosition { get; set; }
+    public List<Poi> VisitedPois { get; set; } = new List<Poi>();
+    public bool Completed { get; set; }
+    
+    public bool HasFoundTarget { get; set; }
+    public Poi? TargetPoi { get; set; }
+
+    public Poi EndPoi { get; set; }
+
+    public Ant(int id, Poi startPosition, Poi endPoi)
+    {
+        Id = id;
+        CurrentPosition = startPosition;
+        StartPosition = startPosition;
+        EndPoi = endPoi;
+        VisitedPois.Add(startPosition);
+    }
+}
+
+public class AntColonyAlgorithm2
+{
+    private Edge[] _edges;
+    private readonly Dictionary<int, Poi> _pois;
+    private readonly Dictionary<(int from, int to), double> _pheromones;
+    private readonly Random _random;
+
+    
+    private readonly double _alpha;
+    private readonly double _beta;
+    private readonly double _gamma;
+    private readonly double _evaporation;
+    private readonly double _q;
+    private int _maxAntSteps = 300;
+
+    private int _antCount = 500;
+
+    public AntColonyAlgorithm2(IOptions<AntColonyConfiguration> configuration)
+    {
+        _random = new Random();
+        _pheromones = new Dictionary<(int, int), double>();
+        _pois = new Dictionary<int, Poi>();
+        
+        _alpha = configuration.Value.Alpha;
+        _beta = configuration.Value.Beta;
+        _gamma = configuration.Value.Gamma;
+        _evaporation = configuration.Value.Evaporation;
+        _q = configuration.Value.Q;
+    }
+
+    private void ExtractPois()
+    {
+        foreach (var edge in _edges)
+        {
+            if (!_pois.ContainsKey(edge.From.Id))
+                _pois[edge.From.Id] = edge.From;
+            if (!_pois.ContainsKey(edge.To.Id))
+                _pois[edge.To.Id] = edge.To;
+        }
+    }
+
+    private void InitializePheromones()
+    {
+        foreach (var edge in _edges)
+        {
+            var key1 = (edge.From.Id, edge.To.Id);
+            var key2 = (edge.To.Id, edge.From.Id);
+            
+            _pheromones[key1] = 1.0;
+            _pheromones[key2] = 1.0;
+        }
+    }
+
+    private double CalculateDistance(Poi from, Poi to)
+    {
+        var dx = from.Point.X - to.Point.X;
+        var dy = from.Point.Y - to.Point.Y;
+        return Math.Sqrt(dx * dx + dy * dy);
+    }
+
+    private double GetWeightValue(Poi to)
+    {
+        return to.Weight > 0 ? to.Weight * 10 : 1.0;
+        // var weightInfluence = to.Weight > 0 ? to.Weight : 1.0;
+        // return weightInfluence / (distance + 0.1); // +0.1 чтобы избежать деления на 0
+    }
+
+    private Dictionary<Poi, double> GetProbabilities(Poi current, List<Poi> visited, List<Edge> availableEdges)
+    {
+        var probabilities = new Dictionary<Poi, double>();
+        var total = 0.0;
+
+        foreach (var edge in availableEdges)
+        {
+            var neighbor = edge.To.Id == current.Id ? edge.From : edge.To;
+            
+            // Пропускаем уже посещенные точки
+            if (visited.Any(p => p.Id == neighbor.Id))
+                continue;
+
+            var pheromoneKey = (current.Id, neighbor.Id);
+            var pheromone = _pheromones.ContainsKey(pheromoneKey) ? _pheromones[pheromoneKey] : 0.1;
+            
+            var distance = CalculateDistance(edge.From, edge.To);
+            
+            var weight = GetWeightValue(neighbor);
+            
+            var probability = Math.Pow(pheromone, _alpha) * 
+                            Math.Pow(distance, _beta) *
+                            Math.Pow(weight, _gamma);
+            
+            probabilities[neighbor] = probability;
+            total += probability;
+        }
+
+        // Нормализуем вероятности
+        if (total > 0)
+        {
+            foreach (var poi in probabilities.Keys.ToList())
+            {
+                probabilities[poi] /= total;
+            }
+        }
+
+        return probabilities;
+    }
+
+    private Poi SelectNextPoi(Poi current, Dictionary<Poi, double> probabilities)
+    {
+        if (probabilities.Count == 0)
+            return null;
+
+        var randomValue = _random.NextDouble();
+        var cumulative = 0.0;
+
+        foreach (var (poi, probability) in probabilities)
+        {
+            cumulative += probability;
+            if (randomValue <= cumulative)
+                return poi;
+        }
+
+        // Если что-то пошло не так, возвращаем первую точку
+        return probabilities.Keys.First();
+    }
+
+    public List<Ant> Run(Edge[] edges, int maxIterations = 10000)
+    {
+        _edges = edges;
+        
+        InitializePheromones();
+        ExtractPois();
+
+        var ants = InitAnts(); 
+
+        for (var iteration = 0; iteration < maxIterations; iteration++)
+        {
+            // Движение муравьев
+            var iterator = 0;
+            foreach (var ant in ants.Where(a => !a.Completed))
+            {
+                MoveAnt(ant);
+            }
+
+            // Обновление феромонов
+            UpdatePheromones(ants);
+
+            // Испарение феромонов
+            EvaporatePheromones();
+
+            // Проверка завершения
+            if (ants.All(a => a.Completed || a.VisitedPois.Count >= _maxAntSteps))
+                break;
+        }
+
+        var preparedResult = PrepareResult(ants);
+
+        var generator = new PheromoneGraphVisualizer(_edges, _pheromones);
+        generator.SaveToFile("test.svg");
+        
+        // 1. Вернуть самые ОФЕРАМОННЕНЫЕ пути !@#$%
+        // 2. Сделать так, чтобы муравьи шли до своей конечной точки+
+        return ants;
+    }
+
+    private List<Path> PrepareResult(List<Ant> ants)
+    {
+        var pathList = new List<Path>();
+        
+        foreach (var antThatFoundTarget in ants.Where(it => it.HasFoundTarget))
+        {
+            var path = new Path();
+            path.Start = antThatFoundTarget.VisitedPois[0];
+            path.End = antThatFoundTarget.EndPoi;
+            path.Points =  antThatFoundTarget.VisitedPois.Skip(1).ToList();
+            pathList.Add(path);
+        }
+        
+        return pathList;
+    }
+
+    private List<int> GetPoolOfEndPoints(List<Edge> notNullFromWeights)
+    {
+        var listIds =  new List<int>();
+        var minWeight = notNullFromWeights.MinBy(it => it.From.Weight).From.Weight;
+        var coeff = 1 / minWeight;
+
+        foreach (var weight in notNullFromWeights)
+        {
+            for (var i = 0; i < weight.From.Weight * coeff; i++)
+            {
+                listIds.Add(weight.From.Id);
+            }
+        }
+        
+        return listIds;
+    }
+
+    private List<Ant> InitAnts()
+    {
+        // Было
+        // var ants = new List<Ant>();
+        //
+        // var startPoi = _edges.MaxBy(it => it.From.Weight).From;
+        //
+        // // Создаем муравьев
+        // for (var i = 0; i < _antCount; i++)
+        // {
+        //     ants.Add(new Ant(i, startPoi));
+        // }
+        
+        // Стало
+        var ants = new List<Ant>();
+        var notNullFromWeights = _edges.Where(it => it.From.Weight != 0).ToList();
+        
+        // условно 2.6
+        var weightSum = notNullFromWeights.Sum(it => it.From.Weight);
+        
+        // условно 1000 / 2.6 = 385
+        var oneAntCost = _antCount / weightSum;
+
+        var endPointPool = GetPoolOfEndPoints(notNullFromWeights);
+        
+        
+        foreach (var distanceWeight in notNullFromWeights)
+        {
+            var antCount = (int)Math.Ceiling(oneAntCost * distanceWeight.From.Weight);
+            var startPoi = _edges.FirstOrDefault(it => it.From.Id == distanceWeight.From.Id).From;
+            var endPointId = endPointPool[_random.Next(0,  endPointPool.Count - 1)];
+
+            while (endPointId == startPoi.Id)
+            {
+                endPointId = endPointPool[_random.Next(0,  endPointPool.Count - 1)];
+            }
+            
+            var endPoi = _edges.FirstOrDefault(it => it.From.Id == endPointId).From;
+
+            // Создаем муравьев
+            for (var i = 0; i < antCount; i++)
+            {
+                ants.Add(new Ant(i, startPoi, endPoi));
+            }
+        }
+
+        return ants;
+    }
+
+    private void MoveAnt(Ant ant)
+    {
+        var current = ant.CurrentPosition;
+        
+        // Получаем доступные ребра из текущей позиции
+        var availableEdges = _edges.Where(e => 
+            e.From.Id == current.Id || e.To.Id == current.Id).ToList();
+
+        // Получаем вероятности перехода
+        var probabilities = GetProbabilities(current, ant.VisitedPois, availableEdges);
+        
+        if (probabilities.Count == 0)
+        {
+            // Некуда идти - завершаем маршрут
+            ant.Completed = true;
+            return;
+        }
+
+        // Выбираем следующую точку
+        var nextPoi = SelectNextPoi(current, probabilities);
+        
+        if (nextPoi != null)
+        {
+            ant.CurrentPosition = nextPoi;
+            ant.VisitedPois.Add(nextPoi);
+            
+            // Проверяем, нашли ли мы целевую точку (с весом)
+            if (nextPoi.Id == ant.EndPoi.Id)
+            {
+                ant.Completed = true;
+                ant.HasFoundTarget = true;
+                ant.TargetPoi = nextPoi;
+            }
+        }
+    }
+
+    private void UpdatePheromones(List<Ant> ants)
+    {
+        foreach (var ant in ants.Where(a => a.Completed))
+        {
+            var pathQuality = CalculatePathQuality(ant);
+            
+            for (var i = 0; i < ant.VisitedPois.Count - 1; i++)
+            {
+                var from = ant.VisitedPois[i];
+                var to = ant.VisitedPois[i + 1];
+                
+                var key1 = (from.Id, to.Id);
+                var key2 = (to.Id, from.Id);
+                
+                if (_pheromones.ContainsKey(key1))
+                    _pheromones[key1] += _q / pathQuality;
+                
+                if (_pheromones.ContainsKey(key2))
+                    _pheromones[key2] += _q / pathQuality;
+            }
+        }
+    }
+
+    private double CalculatePathQuality(Ant ant)
+    {
+        if (ant.VisitedPois.Count < 2) return double.MaxValue;
+        
+        double totalDistance = 0;
+        for (var i = 0; i < ant.VisitedPois.Count - 1; i++)
+        {
+            totalDistance += CalculateDistance(ant.VisitedPois[i], ant.VisitedPois[i + 1]);
+        }
+
+        double weight;
+        // Качество пути: расстояние + штраф за длинные маршруты
+        if (ant.TargetPoi?.Weight == null || ant.TargetPoi?.Weight == 0)
+        {
+            weight = 1;
+        }
+        else
+        {
+            weight = (double)ant.TargetPoi?.Weight;
+        }
+
+        var targetWeightBonus = weight;
+        return totalDistance * targetWeightBonus;
+    }
+
+    private void EvaporatePheromones()
+    {
+        var keys = _pheromones.Keys.ToList();
+        foreach (var key in keys)
+        {
+            _pheromones[key] *= (1 - _evaporation);
+            // Минимальный уровень феромонов
+            if (_pheromones[key] < 0.1)
+                _pheromones[key] = 0.1;
+        }
+    }
+}
