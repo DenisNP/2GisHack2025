1c1f6a5d752548686faecd21310ed05b6c78a84a
Denis Peshekhonov <denis@peshekhonov.ru>
2025-10-05 08:35:30 +0300

    поправил наложение точек

--- frontend/src/utils/getDistance.ts
+++ frontend/src/utils/getDistance.ts
@@ -1,6 +1,7 @@
 import { GeoPoint } from '../types/GeoPoint';
+import { Point } from '../types/Point';
 
-export default (p1: GeoPoint, p2: GeoPoint): number => {
+export const geoDistance = (p1: GeoPoint, p2: GeoPoint): number => {
     const R = 6378137; // Earth’s mean radius in meter
     const dLat = rad(p2.lat - p1.lat);
     const dLong = rad(p2.lng - p1.lng);
@@ -13,6 +14,12 @@ export default (p1: GeoPoint, p2: GeoPoint): number => {
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 
     return R * c;
+};
+
+export const distance = (p1: Point, p2: Point): number => {
+    const dx = p1.x - p2.x;
+    const dy = p1.y - p2.y;
+    return Math.sqrt(dx * dx + dy * dy);
 };
 
 const rad = function(x: number): number {
