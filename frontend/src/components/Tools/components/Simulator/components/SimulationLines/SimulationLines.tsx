8fe669d4022d46ccc0bf3a2094e0695da9e31498
Denis Peshekhonov <denis@peshekhonov.ru>
2025-10-09 21:14:16 +0300

    points opacity 3

--- frontend/src/components/Tools/components/Simulator/components/SimulationLines/SimulationLines.tsx
+++ frontend/src/components/Tools/components/Simulator/components/SimulationLines/SimulationLines.tsx
@@ -27,7 +27,7 @@ export const SimulationLines: React.FC = () => {
                     const point = points[i];
 
                     const size = 4; // Math.max(1, Math.min(point.weight * 5, 5));
-                    const minOpacity = 0.6;
+                    const minOpacity = 0.2;
                     const opacity = Math.max(minOpacity, minOpacity + point.weight * (1 - minOpacity));
                     const div = document.createElement('div');
                     div.style.cssText = `
